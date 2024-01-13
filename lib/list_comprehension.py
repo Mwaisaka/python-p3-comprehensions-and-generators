fa4d2f0cd77f4cd19212c121da7998bbc48d6c3b
Frank <frankkashere@gmail.com>
2024-01-13 09:34:10 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # if([(num_list/2) for num_list in num_list]):
+    #     return []
+    # elif([(num_list/2) for num_list in num_list]&(num_list>0)):
+    #     return num_list
+    return [n for n in num_list if n%2==0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
